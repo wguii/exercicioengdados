dfcccdc680d7bc142f334a07f37295be9a2dc60f
wguii <133844217+wguii@users.noreply.github.com>
2025-09-04 18:06:23 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+#Script para facilitar as ações no Dockerfile, utilizando-o como um entrypoint pro container do SQL Server
+
 #!/bin/bash
 # Sobe o SQL Server em segundo plano
 /opt/mssql/bin/sqlservr &
@@ -5,7 +7,7 @@
 # Espera o SQL Server ficar pronto
 sleep 20
 
-# Executa o script SQL
+# Executa o script SQL (aqui rodamos as instruções do "create_ddl_dml.sql", criando as tabelas, populando, etc.)
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -i /scripts/create_ddl_dml.sql
 
 # Mantém o container rodando
